b3e49164190ec48af37e74aa47757d6dc5fff412
PiotrO9 <piotr.ostrowski02@wp.pl>
2025-04-25 14:59:03 +0200

    feat: add new price table for products and also add cart tables into database

---
+++ prisma/migrations/20250425125838_add_price_model/migration.sql
@@ -0,0 +1,69 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `price` on the `ProductVariant` table. All the data in the column will be lost.
+
+*/
+-- CreateEnum
+CREATE TYPE "DiscountType" AS ENUM ('FIXED', 'PERCENTAGE');
+
+-- AlterTable
+ALTER TABLE "ProductVariant" DROP COLUMN "price";
+
+-- CreateTable
+CREATE TABLE "Price" (
+    "id" UUID NOT NULL,
+    "productVariantId" UUID NOT NULL,
+    "basePrice" DOUBLE PRECISION NOT NULL,
+    "salePrice" DOUBLE PRECISION,
+    "discountType" "DiscountType",
+    "discountValue" DOUBLE PRECISION,
+    "startDate" TIMESTAMP(3),
+    "endDate" TIMESTAMP(3),
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Price_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Cart" (
+    "id" UUID NOT NULL,
+    "userId" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Cart_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CartItem" (
+    "id" UUID NOT NULL,
+    "cartId" UUID NOT NULL,
+    "productVariantId" UUID NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "CartItem_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Price_productVariantId_key" ON "Price"("productVariantId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Cart_userId_key" ON "Cart"("userId");
+
+-- AddForeignKey
+ALTER TABLE "Price" ADD CONSTRAINT "Price_productVariantId_fkey" FOREIGN KEY ("productVariantId") REFERENCES "ProductVariant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Cart" ADD CONSTRAINT "Cart_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CartItem" ADD CONSTRAINT "CartItem_cartId_fkey" FOREIGN KEY ("cartId") REFERENCES "Cart"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CartItem" ADD CONSTRAINT "CartItem_productVariantId_fkey" FOREIGN KEY ("productVariantId") REFERENCES "ProductVariant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
